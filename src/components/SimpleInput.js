483f0d55a993b0907a85c8fe2fb5a8de41fc3a67
adilahamad786 <adilahamad.personally@gmail.com>
2022-10-08 13:06:52 +0530

    Validation field when form submitted.

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,10 +1,32 @@
+import { useState } from "react";
+
 const SimpleInput = (props) => {
+
+  const [enteredName, setEnteredName] = useState('');
+  const [enteredNameIsValid, setEnteredNameIsValid] = useState(true);
+
+  const onChangeHandler = (event) => {
+    setEnteredName(event.target.value);
+  }
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    if (enteredName.trim() === '') {
+      setEnteredNameIsValid(false)
+      return;
+    }
+    setEnteredNameIsValid(true);
+    setEnteredName('');
+  }
+
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div className='form-control'>
         <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+        <input onChange={onChangeHandler} value={enteredName} type='text' id='name' />
       </div>
+      { !enteredNameIsValid && <p className="error-text">Name must not be Empty!</p> }
       <div className="form-actions">
         <button>Submit</button>
       </div>
